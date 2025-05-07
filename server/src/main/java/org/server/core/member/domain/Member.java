e6cd81d00bb165d7a5fb604925b3962ea425b64e
seomoon <sunghoon1216@naver.com>
2025-05-07 16:10:47 +0900

    feat: Position(직무) 관련 데이터 enum 클래스에 열거
    직무별 통계를 위해 Member에 추가할 Position 관련 내용을 Enum 클래스에
    열거

---
+++ server/src/main/java/org/server/core/member/domain/Member.java
@@ -0,0 +1,11 @@
+package org.server.core.member.domain;
+
+import jakarta.persistence.Entity;
+import lombok.NoArgsConstructor;
+import org.server.global.common.BaseEntity;
+
+@Entity
+@NoArgsConstructor
+public class Member extends BaseEntity {
+
+}
