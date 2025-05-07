8af721ea7a0fff99b341b32944f0b7bce19804a3
SighingOwl <whddms1208@gmail.com>
2025-05-08 07:03:53 +0900

    Create get metric feature

--- server/src/main/java/org/server/core/metric/api/MetricApi.java
+++ server/src/main/java/org/server/core/metric/api/MetricApi.java
@@ -2,12 +2,15 @@ package org.server.core.metric.api;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.server.core.metric.api.payload.MetricGetRequest;
 import org.server.core.metric.api.payload.MetricInsertRequest;
+import org.server.core.metric.domain.ActiveTimeEntry;
 import org.server.core.metric.service.MetricService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -21,5 +24,17 @@ public class MetricApi {
     public void insert(@RequestBody MetricInsertRequest request) {
         log.info("Insert metric: {}", request);
         metricService.save(request.userId(), request.toDomain(), request.toMetricMetadata());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ActiveTimeEntry>> get(@RequestBody MetricGetRequest request) {
+        log.info("Get metric: {}", request);
+        List<ActiveTimeEntry> activeTime = metricService.find(request.userId(), request.term());
+
+        var response = ResponseEntity
+                .status(HttpStatus.OK)
+                .body(activeTime);
+
+        return response;
     }
 }
--- server/src/main/java/org/server/core/metric/service/MetricService.java
+++ server/src/main/java/org/server/core/metric/service/MetricService.java
@@ -1,14 +1,18 @@
 package org.server.core.metric.service;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.server.core.metric.domain.Domain;
-import org.server.core.metric.domain.Metric;
-import org.server.core.metric.domain.MetricMetadata;
-import org.server.core.metric.domain.MetricRepository;
+import org.server.core.metric.domain.*;
+import org.server.core.site.domain.SiteDomain;
 import org.server.core.site.domain.SiteDomainRepository;
 import org.server.core.site.infra.ExternalSiteClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.*;
 
 @Service
 @Transactional
@@ -24,5 +28,75 @@ public class MetricService {
                 .orElseGet(() -> siteDomainRepository.save(externalSiteClient.getSiteDomain(domain)));
 
         metricRepository.save(new Metric(userId, siteDomain, metric));
+    }
+
+    public List<ActiveTimeEntry> find(Long userId, String term) {
+        LocalDateTime start = getStartTime(term);
+
+        List<Metric> userMetrics = metricRepository.findAllByMemberIdAndRequestAtAfter(userId, start.toInstant(ZoneOffset.of("+09:00")));
+        if (userMetrics.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<ActiveTimeEntry> activeTime = calculateActiveTime(userMetrics);
+
+        return activeTime;
+    }
+
+    public LocalDateTime getStartTime(String term) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime start = switch (term) {
+            case "day" -> // 지난 24시간이 아닌 당일 0시부터
+                    now.toLocalDate().atStartOfDay();
+            case "week" -> now.minusWeeks(1);
+            case "month" -> now.minusMonths(1);
+            default -> throw new IllegalArgumentException("Invalid time parameter : " + term);
+        };
+
+        return start;
+    }
+
+    public List<ActiveTimeEntry> calculateActiveTime(List<Metric> metrics) {
+        Map<SiteDomain, DomainTimeEntry> domainTime = new HashMap<>();
+
+        // 각 사이트 도메인별로 요청 시간을 계산
+        // path에 따라 요청 시간 계산 로직 추가 필요
+        for (Metric metric : metrics) {
+            SiteDomain siteDomain = metric.getSiteDomain();
+            LocalDateTime requestAt = LocalDateTime.ofInstant(metric.getMetadata().requestAt(), ZoneOffset.of("+09:00"));
+
+            domainTime.putIfAbsent(siteDomain, new DomainTimeEntry(requestAt, 0L));
+
+            long duration = java.time.Duration.between(domainTime.get(siteDomain).getRequestAt(), requestAt).toSeconds();
+            domainTime.get(siteDomain).addDuration(duration);
+            domainTime.get(siteDomain).setRequestAt(requestAt);
+        }
+
+        List<ActiveTimeEntry> activeTime = new ArrayList<>();
+        for (Map.Entry<SiteDomain, DomainTimeEntry> entry : domainTime.entrySet()) {
+            activeTime.add(new ActiveTimeEntry(entry.getKey(), entry.getValue().getDuration()));
+        }
+
+        return activeTime;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    private static class DomainTimeEntry {
+        private LocalDateTime requestAt;
+        private long duration;
+
+        public DomainTimeEntry(LocalDateTime requestAt, long duration) {
+            this.requestAt = requestAt;
+            this.duration = duration;
+        }
+
+        public void setRequestAt(LocalDateTime requestAt) {
+            this.requestAt = requestAt;
+        }
+
+        public void addDuration(long duration) {
+            this.duration += duration;
+        }
     }
 }
