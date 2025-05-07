de245b43bd0e0031782c7eebb314fb4ec59c0f54
seomoon <sunghoon1216@naver.com>
2025-05-07 16:19:31 +0900

    feat: Member Entity, Repository 구조 작성
    Member 엔티티의 구조를 작성하고, JPA repository를 상속받는 인터페이스
    추가

--- server/src/main/java/org/server/core/member/domain/Member.java
+++ server/src/main/java/org/server/core/member/domain/Member.java
@@ -1,11 +1,24 @@
 package org.server.core.member.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.server.global.common.BaseEntity;
 
+@Getter
 @Entity
 @NoArgsConstructor
 public class Member extends BaseEntity {
 
+    @Enumerated(EnumType.STRING)
+    private OAuthProvider oAuthProvider;
+
+    @Enumerated(EnumType.STRING)
+    private Position positon;
+
+    private String profileUrl;
+
+    private String nickname;
 }
