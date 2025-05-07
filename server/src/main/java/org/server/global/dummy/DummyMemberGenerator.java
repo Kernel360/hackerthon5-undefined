a847116a668cf57cf2c46db82bd6bc31e29b26d7
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-05-07 18:44:53 +0900

    feat: local, dev profile에서 동작하는 가짜 사용자 생성 코드 추가

---
+++ server/src/main/java/org/server/global/dummy/DummyMemberGenerator.java
@@ -0,0 +1,34 @@
+package org.server.global.dummy;
+
+import java.util.stream.IntStream;
+import lombok.RequiredArgsConstructor;
+import org.server.core.member.domain.Member;
+import org.server.core.member.domain.MemberRepository;
+import org.server.core.member.domain.OAuthProvider;
+import org.server.core.member.domain.Position;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@RequiredArgsConstructor
+@Profile({"local", "dev"})
+public class DummyMemberGenerator {
+
+    private final MemberRepository memberRepository;
+
+    @Bean
+    ApplicationRunner applicationRunner() {
+        return args -> memberRepository.saveAll(
+                IntStream.iterate(1, i -> i + 1)
+                        .mapToObj(i -> new Member(
+                                OAuthProvider.GITHUB,
+                                Position.BACKEND,
+                                "https://www.naver.com",
+                                "nickname" + i
+                        ))
+                        .toList()
+        );
+    }
+}
