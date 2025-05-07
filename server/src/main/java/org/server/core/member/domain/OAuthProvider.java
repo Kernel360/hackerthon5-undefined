afb701988b11cace6b1c5c905e766785b7ea6202
seomoon <sunghoon1216@naver.com>
2025-05-07 18:48:04 +0900

    feat: OAuthProvider 요청에 따라 다르게 가져오기

--- server/src/main/java/org/server/core/member/domain/OAuthProvider.java
+++ server/src/main/java/org/server/core/member/domain/OAuthProvider.java
@@ -1,5 +1,18 @@
 package org.server.core.member.domain;
 
 public enum OAuthProvider {
-    GITHUB, KAKAO, GOOGLE       //깃허브 로그인만 구현, 확장성 고려
+    GITHUB, KAKAO, GOOGLE;       //깃허브 로그인만 구현, 확장성 고려
+
+
+    //FIXME: 임시 코드
+    public static OAuthProvider getProvider(String requestUrl) {
+        if (requestUrl.contains("github")) {
+            return GITHUB;
+        } else if (requestUrl.contains("kakao")) {
+            return KAKAO;
+        } else if (requestUrl.contains("google")) {
+            return GOOGLE;
+        }
+        throw new IllegalArgumentException("[ERROR] 지원하지 않는 로그인 방식입니다.");
+    }
 }
