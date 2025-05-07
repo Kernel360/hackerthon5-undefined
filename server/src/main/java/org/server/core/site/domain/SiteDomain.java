e124da163253109284a60ec986488a21445fc1fe
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-05-07 16:43:34 +0900

    refactor: 기본 생성자 이외에 favicon 및 사이트 이름과 관련된 값을 받도록 추가

--- server/src/main/java/org/server/core/site/domain/SiteDomain.java
+++ server/src/main/java/org/server/core/site/domain/SiteDomain.java
@@ -1,17 +1,29 @@
 package org.server.core.site.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.server.global.common.BaseEntity;
 
+@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SiteDomain extends BaseEntity {
 
+    @Column(nullable = false)
     private String url;
     private String favicon;
     private String name;
+
+    public SiteDomain(String url){
+        this(url, "", "");
+    }
+
+    public SiteDomain(String url, String favicon){
+        this(url, favicon, "");
+    }
 
     public SiteDomain(String url, String favicon, String name) {
         this.url = url;
