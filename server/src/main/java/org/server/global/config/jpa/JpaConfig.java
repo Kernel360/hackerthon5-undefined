4077b7c98f699c5d95722f128251d79999dbdf58
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-05-07 15:18:55 +0900

    feat: baseEntity 및 의존성 추가

---
+++ server/src/main/java/org/server/global/config/jpa/JpaConfig.java
@@ -0,0 +1,16 @@
+package org.server.global.config.jpa;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaAuditing
+@EnableTransactionManagement
+@EntityScan(basePackages = "org.server.core")
+@EnableJpaRepositories(basePackages = "org.server")
+public class JpaConfig {
+}
+
