ac668fa40c4e9d635c0e288b03745138f1c83843
seomoon <sunghoon1216@naver.com>
2025-05-09 00:13:31 +0900

    refacotr: jwt 생성 방식 약간 수정, 유효기간 1주일 -> 1달

--- server/src/main/java/org/server/core/token/utils/TokenProvider.java
+++ server/src/main/java/org/server/core/token/utils/TokenProvider.java
@@ -1,15 +1,10 @@
 package org.server.core.token.utils;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import javax.crypto.SecretKey;
 import lombok.RequiredArgsConstructor;
-import org.server.core.member.domain.Member;
 import org.server.core.member.domain.UserProfile;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -20,11 +15,9 @@ public class TokenProvider {
     @Value("${jwt.token.secret}")
     private String secretKey;
 
-    private static final Long ACCESS_TOKEN_EXPIRE_TIME_MS = 604800000L; // 1주일
-    private static final String USER_NO_KEY_NAME = "USER_NO";
-    private static final String USER_ID_KEY_NAME = "USER_ID";
-
-    private SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    private static final Long ACCESS_TOKEN_EXPIRE_TIME_MS = 2592000000L; // 한 달
+    private static final String MEMBER_PROFILE_KEY_NAME = "userProfile";
+    private static final String MEMBER_ID_KEY_NAME = "userId";
 
     public Date calcExpiration() {
         Date now = new Date();
@@ -32,18 +25,15 @@ public class TokenProvider {
     }
 
     public String createAccessToken(Long memberId, UserProfile userProfile) {
-
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("id", memberId);
-        claims.put("userProfile", userProfile);
-
-        return Jwts.builder()
-                .claims(claims)
+        String token = Jwts.builder()
+                .claim(MEMBER_ID_KEY_NAME, memberId)
+                .claim(MEMBER_PROFILE_KEY_NAME, userProfile)
                 .issuedAt(new Date())
                 .expiration(calcExpiration())
-                .signWith(
-                        Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)),
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)),
                         SignatureAlgorithm.HS256)
                 .compact();
+
+        return token;
     }
 }
