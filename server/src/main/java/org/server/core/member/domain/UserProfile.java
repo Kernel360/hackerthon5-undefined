14e06a15eac7898519987e1871fa6a105075cc8f
seomoon <sunghoon1216@naver.com>
2025-05-08 15:58:26 +0900

    fix: resolve conflict and import optimizing

--- server/src/main/java/org/server/core/member/domain/UserProfile.java
+++ server/src/main/java/org/server/core/member/domain/UserProfile.java
@@ -1,7 +1,5 @@
 package org.server.core.member.domain;
 
-import java.util.Map;
-
 public record UserProfile(
         String name,
         String id,
--- server/src/main/java/org/server/core/metric/api/MetricApi.java
+++ server/src/main/java/org/server/core/metric/api/MetricApi.java
@@ -1,11 +1,18 @@
 package org.server.core.metric.api;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.server.core.metric.api.payload.MetricInsertRequest;
+import org.server.core.metric.api.payload.request.MetricGetRequest;
+import org.server.core.metric.api.payload.request.MetricInsertRequest;
+import org.server.core.metric.domain.ActiveTimeEntry;
 import org.server.core.metric.service.MetricService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +37,4 @@ public class MetricApi {
 
         return ResponseEntity.status(HttpStatus.OK).body(activeTime);
     }
-    
 }
