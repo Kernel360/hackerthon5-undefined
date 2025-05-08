9232c1e8563a05a56bc1cc18f0bea5454930ac4c
seomoon <sunghoon1216@naver.com>
2025-05-08 18:24:29 +0900

    refactor: 로그인 관련 환경변수 분리

--- server/src/main/java/org/server/core/member/domain/Position.java
+++ server/src/main/java/org/server/core/member/domain/Position.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum Position {
-    NONE("설정 전", 0),
+    NONE("없음", 0),
     BACKEND("백엔드 개발자", 1),
     FRONTEND("프론트엔드 개발자", 2);
 
