7f14ce51b5b641752776bb9d0d07ca2d4b6ac2ee
seomoon <sunghoon1216@naver.com>
2025-05-09 11:41:17 +0900

    fix: token test api 명세 description 수정

--- server/src/main/java/org/server/core/token/api/TokenApiDocs.java
+++ server/src/main/java/org/server/core/token/api/TokenApiDocs.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 public interface TokenApiDocs {
 
-    @Operation(summary = "깃허브 로그인 요청 API", description = "회원가입 혹은 로그인을 요청하고, 토큰을 반환합니다")
-    @ApiResponse(responseCode = "200", description = "로그인이 완료되었습니다.")
+    @Operation(summary = "임시 토큰 발행 요청", description = "테스트를 위해 임시로 토큰을 가져옵니다")
+    @ApiResponse(responseCode = "200", description = "토큰 발행이 완료되었습니다")
     String generateTokenForTest();
 }
