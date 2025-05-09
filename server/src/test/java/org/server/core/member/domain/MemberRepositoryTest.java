9b656ef6e38c14e0a6909bc80a519f44192681ac
seomoon <sunghoon1216@naver.com>
2025-05-09 11:09:29 +0900

    fix: test successful

--- server/src/test/java/org/server/core/member/domain/MemberRepositoryTest.java
+++ server/src/test/java/org/server/core/member/domain/MemberRepositoryTest.java
@@ -37,7 +37,7 @@ class MemberRepositoryTest {
 
         //then
         Assertions.assertThat(maybeMember.isPresent()).isTrue();
-        Assertions.assertThat(maybeMember.get().getNickname()).isEqualTo("ttt");
+        Assertions.assertThat(maybeMember.get().getNickname()).isEqualTo("testName");
     }
 
     @Test
